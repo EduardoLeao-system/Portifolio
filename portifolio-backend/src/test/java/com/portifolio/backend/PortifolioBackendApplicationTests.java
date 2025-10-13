f1feef060065d80162ebc66220ce79ce7e235902
EduardoLeao-system <carloseduardomeirelesleao@gmail.com>
2025-10-13 09:29:44 -0400

    adicionando pasta backend, configurando projeto e dependecias do spring

---
+++ portifolio-backend/src/test/java/com/portifolio/backend/PortifolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
